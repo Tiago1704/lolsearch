ab70115bb7023e925ec1b8b49a59133dae280411
Tiago Ibacache <tibacache@proguidemc.com>
2022-09-04 18:29:07 -0300

    Cambios containers e itemspage.

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Container } from "@mui/material";
+import { createStyles, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => createStyles({
+  container: {
+      height: '100%',
+      overflow: 'visible',
+      backgroundColor: "#BDBDBD",
+      minHeight: 770,
+      width: '100%',
+  },    
+}));
 
 const ErrorPage = () => {
+  const classes = useStyles();
+
     return(
-        <div color = "#000000" className="ion-text-center">
-          <h4 className = 'ion-default'> Proximamente </h4>
-        </div>
+      <Container className={classes.container}>
+          dasadas
+      </Container>
       );
 }
+
+// useEffect(() => {
+//   searchChamps("Akali").then((response) => console.log(response))
+//   champsArrays().then(response => console.log(response))
+// },[])
 
 export default ErrorPage;
