77699f9801fcf7cc7a68783dfa31d8c7b3a13003
Juan Rivero <41085597+JuanInhale@users.noreply.github.com>
2022-09-04 19:06:44 -0300

    Update Inicio.js

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,28 +1,7 @@
 import React from "react";
 import { Container, Typography, TextField, Button } from '@mui/material';
 import { createStyles, makeStyles } from "@material-ui/core"
-
-//import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-const images = [
-    {
-        label: "Imagen de prueba",
-        imgPath: "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
-    },
-
-];
+import Swipeable from "../components/Swipeable";
 
 const useStyles = makeStyles((theme) => createStyles({    
     title: {
@@ -43,6 +22,7 @@ const Inicio = () => {
             <Typography className={classes.title}>
             Pagina de inicio
             </Typography>
+            <Swipeable/>
         </Container>
     )
 } 
